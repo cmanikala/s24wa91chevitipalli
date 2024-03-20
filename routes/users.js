e05eec0cb93718b05cb0b4731bef96e4656ec055
cmanikala <156376320+cmanikala@users.noreply.github.com>
2024-03-20 10:40:32 -0500

    c3

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const product =1;
+const factor =1;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor++;
+  product = product*factor;
+  res.send(`product is ${product}`);
 });
 
 module.exports = router;
